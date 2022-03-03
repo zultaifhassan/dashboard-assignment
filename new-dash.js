24deeba9e402eef28768aa407247143aea200445
zultaifhassan <95405580+zultaifhassan@users.noreply.github.com>
2022-03-03 11:34:36 +0500

    Add files via upload

---
+++ new-dash.js
@@ -0,0 +1,39 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.drop-icon')) {
+      var dropdowns = document.getElementsByClassName("dropdown-information");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+
+  function myFunction1() {
+    document.getElementById("myDrop2").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.drop-icon2')) {
+      var dropdowns = document.getElementsByClassName("log-outdrop");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
